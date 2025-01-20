f38d8805ce0d2ce6659c34f8ea77a939a3f9fd9a
Pooja Patel <poojapatel15115@gmail.com>
2025-01-20 09:25:49 +0530

    images

--- src/lec2001/useeffect.jsx
+++ src/lec2001/useeffect.jsx
@@ -1,14 +1,14 @@
 //implement image changer with useeffect  
 //create a list of 4 images: Change image at interval of 4 seconds
-
+"../../public/"
 import React, { useState, useEffect } from 'react';
 
 const ImageChanger = () => {
   const images = [
-    'https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?cs=srgb&dl=pexels-anjana-c-169994-674010.jpg&fm=jpg',
-    'https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-1080x675.jpg',
-    'https://i0.wp.com/picjumbo.com/wp-content/uploads/beautiful-fall-waterfall-free-image.jpeg?w=600&quality=80',
-    'https://create.microsoft.com/_next/image?url=https%3A%2F%2Fcdn.create.microsoft.com%2Fimages%2Fimage-creator-T03_cat.webp&w=1920&q=90',
+    'bird.jpg',
+    'kanha.jpg',
+    'girl.jpg',
+    'cat.webp',
   ];
 
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
@@ -22,7 +22,13 @@ const ImageChanger = () => {
   }, [images.length]);
 
   return (
-    <div style={{ width: '600px', height: '400px', overflow: 'hidden', position: 'relative' }}>
+    <div style={{ width: '600px', 
+        height: '400px', 
+        overflow: 'hidden', 
+        display: 'flex', 
+        justifyContent: 'center', 
+        alignItems: 'center', 
+        position: 'relative'}}>
       <img
         src={images[currentImageIndex]}
         alt={`Image ${currentImageIndex + 1}`}
